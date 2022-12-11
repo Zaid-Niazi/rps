4c84c76e9eef9edb4da5a4c609522a96e7b607fc
vazcon <zaidniaazi@gmail.com>
2022-12-11 09:00:46 +0530

    create first working logic

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 
 
-playRound();
 
-function playRound(playerSelection, computerChoice){
+const playerSelection = prompt("Choose").toLowerCase();
+const possibleChoices = ["rock", "paper", "scissor"]
 
-    if(playerSelection === computerChoice){
+
+
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection === computerSelection){
         console.log("Tie")
     }else{
-          if(playerSelection === "rock" && computerChoice === "scissor" || playerSelection === "scissor" && computerChoice === "paper" ||playerSelection === "paper" && computerChoice === "rock"){
+          if(playerSelection === "rock" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "paper" ||playerSelection === "paper" && computerSelection === "rock"){
             console.log("Player Wins")
           }
-         else if(playerSelection === "rock" && computerChoice === "scissor" || playerSelection === "scissor" && computerChoice === "paper" ||playerSelection === "paper" && computerChoice === "rock"){
+         else if(computerSelection === "rock" && playerSelection === "scissor" || computerSelection === "scissor" && playerSelection === "paper" ||computerSelection === "paper" && playerSelection === "rock"){
             console.log("Computer Wins")
          }
 
     }
 }
 
-const playerSelection = prompt("Choose").toLowerCase();
 
-const computerSelection =  getComputerChoice(){
+
+
+const computerSelection =  getcomputerSelection()
+
+function getcomputerSelection(){
     return possibleChoices[Math.floor(Math.random()*3)]
 }
 
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
 
 
 
+/* create getcomputerSelection that will choose a random number and draw an item from possibleChoices array. 
 
-
-/* create getComputerChoice that will choose a random number and draw an item from possibleChoices array. 
-
-create a function that plays one round of the game named playRound with two parameters - playerSelection and computerChoice
+create a function that plays one round of the game named playRound with two parameters - playerSelection and computerSelection
 
 ask player for choice using a prompt and make it case insensitive
 
