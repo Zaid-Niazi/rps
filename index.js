26e926b0af5109546d776794d1def32d4da7ebe5
vazcon <zaidniaazi@gmail.com>
2022-12-11 08:24:25 +0530

    create core logic

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-alert("hi")
+
+
+playRound();
+
+function playRound(playerSelection, computerChoice){
+
+    if(playerSelection === computerChoice){
+        console.log("Tie")
+    }else{
+          if(playerSelection === "rock" && computerChoice === "scissor" || playerSelection === "scissor" && computerChoice === "paper" ||playerSelection === "paper" && computerChoice === "rock"){
+            console.log("Player Wins")
+          }
+         else if(playerSelection === "rock" && computerChoice === "scissor" || playerSelection === "scissor" && computerChoice === "paper" ||playerSelection === "paper" && computerChoice === "rock"){
+            console.log("Computer Wins")
+         }
+
+    }
+}
+
+const playerSelection = prompt("Choose").toLowerCase();
+
+const computerSelection =  getComputerChoice(){
+    return possibleChoices[Math.floor(Math.random()*3)]
+}
+
+
+
+
+
+
+/* create getComputerChoice that will choose a random number and draw an item from possibleChoices array. 
+
+create a function that plays one round of the game named playRound with two parameters - playerSelection and computerChoice
+
+ask player for choice using a prompt and make it case insensitive
+
+
+
+
+*/
+
