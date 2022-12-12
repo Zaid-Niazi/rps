99ded02e74a85ea0942ed3afab4c17f5eff06c9a
vazcon <zaidniaazi@gmail.com>
2022-12-12 07:50:01 +0530

    Fully Working except points

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 
 
-
-const playerSelection = prompt("Choose").toLowerCase();
-const possibleChoices = ["rock", "paper", "scissor"]
+const possibleChoices = ["rock", "paper", "scissor"];
 
 
+function playRound(playerSelection, computerSelection) {
+   let playerPoints = []
+   let compPoints = [] 
+  if (playerSelection === computerSelection){
+   alert("Tie");
+   
 
-function playRound(playerSelection, computerSelection){
 
-    if(playerSelection === computerSelection){
-        console.log("Tie")
-    }else{
-          if(playerSelection === "rock" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "paper" ||playerSelection === "paper" && computerSelection === "rock"){
-            console.log("Player Wins")
-          }
-         else if(computerSelection === "rock" && playerSelection === "scissor" || computerSelection === "scissor" && playerSelection === "paper" ||computerSelection === "paper" && playerSelection === "rock"){
-            console.log("Computer Wins")
-         }
+}else{
+   
+   if(playerSelection === "rock" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "paper" ||playerSelection === "paper" && computerSelection === "rock"){
+      alert ("Player Wins");
+   
 
-    }
+   }
+      
+   else if(computerSelection === "rock" && playerSelection === "scissor" || computerSelection === "scissor" && playerSelection === "paper" ||computerSelection === "paper" && playerSelection === "rock"){
+      alert("Comp Wins");
+    
+   }
+}
+}
+ 
+
+
+
+
+function game(){
+   for(let i =1; i<6; i++){
+    
+   const playerSelection = prompt("Choose").toLowerCase();
+   var computerSelection = possibleChoices[Math.floor(Math.random()*3)];
+   console.log(playRound(playerSelection, computerSelection));
+   
+   }
 }
 
-
-
-
-const computerSelection =  getcomputerSelection()
-
-function getcomputerSelection(){
-    return possibleChoices[Math.floor(Math.random()*3)]
-}
-
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
-
-
-
-/* create getcomputerSelection that will choose a random number and draw an item from possibleChoices array. 
-
-create a function that plays one round of the game named playRound with two parameters - playerSelection and computerSelection
-
-ask player for choice using a prompt and make it case insensitive
-
-
-
-
-*/
-
+game();
