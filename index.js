04bc8baed44cc29c5c278b122299636b5f6494bb
vazcon <zaidniaazi@gmail.com>
2022-12-12 08:20:58 +0530

    Add non working scorecards

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ const possibleChoices = ["rock", "paper", "scissor"];
 
 
 function playRound(playerSelection, computerSelection) {
-   let playerPoints = []
-   let compPoints = [] 
+ 
   if (playerSelection === computerSelection){
-   alert("Tie");
+   return("Tie");
    
 
 
 }else{
    
    if(playerSelection === "rock" && computerSelection === "scissor" || playerSelection === "scissor" && computerSelection === "paper" ||playerSelection === "paper" && computerSelection === "rock"){
-      alert ("Player Wins");
-   
+      return ("Player Wins");
 
    }
       
    else if(computerSelection === "rock" && playerSelection === "scissor" || computerSelection === "scissor" && playerSelection === "paper" ||computerSelection === "paper" && playerSelection === "rock"){
-      alert("Comp Wins");
-    
+      return("Comp Wins");
    }
 }
 }
@@ -31,13 +28,34 @@ function playRound(playerSelection, computerSelection) {
 
 
 function game(){
+
+   var playerPoints =0
+   var compPoints = 0
+    
    for(let i =1; i<6; i++){
     
    const playerSelection = prompt("Choose").toLowerCase();
    var computerSelection = possibleChoices[Math.floor(Math.random()*3)];
    console.log(playRound(playerSelection, computerSelection));
-   
+       if (console.log(playRound(playerSelection, computerSelection)) === "Player Wins"){
+           playerPoints++
+       }
+       else if (console.log(playRound(playerSelection, computerSelection)) === "Comp Wins"){
+           compPoints++
+       }else{
+           
+       }
+       
+                           
    }
+
+ if(playerPoints > compPoints){alert("Player is the Champion !")
+      }else if (compPoints > playerPoints){
+         alert ("Comp is the Champion !")
+      }else{
+         alert("It has been a draw !")
+      }     
+   
 }
 
 game();
